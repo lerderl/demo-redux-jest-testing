bacb0ff74441c4562c463a204ba3dcda1a51b4f3
Joseph Olukunle <josepholukunle1107@gmail.com>
2022-06-25 14:31:40 +0100

    Test completed successfully

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+
 import { store } from './app/store';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+  render(
+    <MemoryRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </MemoryRouter>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(screen.getByText(/learn/i)).toBeInTheDocument();
 });
